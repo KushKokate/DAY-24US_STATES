bd6100ee35237192138d3e8d809272a8f6c6d7a2
Kush <kokatekush@gmail.com>
2024-03-03 01:34:20 +0530

    INITIAL COMMIT

--- main.py
+++ main.py
@@ -14,14 +14,26 @@ turtle.shape(image)
 #turtle.mainloop()
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
+guessed_states = []
+missed_States = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt= "What's the state of your game?").title()
+    if answer_state == "Exit":
+        if answer_state == "Exit":
+            missing_states = []
+            for state in all_states:
+                if state not in guessed_states:
+                    missing_states.append(state)
+            new_data = pandas.DataFrame(missing_states)
+            new_data.to_csv('missing_states.csv')
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[answer_state == data.state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
 
-answer_state = screen.textinput(title="Guess the states", prompt= "What's the state of your game?")
-
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[answer_state == data.state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(state_data.state.item())
 screen.exitonclick()
