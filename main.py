dcca2145539215a7a49c549d69ac4a1d08f932ee
Kush <kokatekush@gmail.com>
2024-03-03 10:35:29 +0530

    INITIAL COMMIT

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import turtle
 import pandas
 screen = turtle.Screen()
 screen.title("US 50 game")
+# set the image of the US to a variable in gif format
 image = "usmaps.gif"
+# using addshape() function where we can add shape
 screen.addshape(image)
 turtle.shape(image)
 
-#def get_mouse_click_coor(x, y):
-#   print(x, y)
-
-#turtle.onscreenclick(get_mouse_click_coor)
-
-#turtle.mainloop()
+# use to read csv data
 data = pandas.read_csv("50_states.csv")
+# convert th data to a list using "to.list()"
 all_states = data.state.to_list()
+#make list for the guessed states and missed states
 guessed_states = []
 missed_States = []
+#using while loop
 while len(guessed_states) < 50:
+    #textinput func helps to take input and makes a field to take input
+    #using to title() to cinvert the input string's first letter to capital even if we write in any manner ex: "angel" or "ANgeLA"-> "Angel"
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt= "What's the state of your game?").title()
     if answer_state == "Exit":
         if answer_state == "Exit":
             missing_states = []
             for state in all_states:
+
                 if state not in guessed_states:
                     missing_states.append(state)
             new_data = pandas.DataFrame(missing_states)
